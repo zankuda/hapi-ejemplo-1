2430626b0e638d5c72e9597207717e2530adf2d1
Harold Fritz <harold.fritz@zankuda.com>
2015-07-31 13:31:26 -0300

    Archivo central que inicializa Hapi y realiza las llamadas a todas las configuraciones de rutas, DB, configuraciones, etc.

---
+++ app.js
@@ -0,0 +1,21 @@
+var config = require('./config')
+var hapi = require('hapi')
+
+var server = new hapi.Server()
+server.connection({ 
+  host: config.server.host,
+  port: config.server.port
+})
+
+var routes = require('./routes')
+routes.init(server, config)
+
+server.start(function () {
+  console.log('Servidor ejecutándose en:', server.info.uri);
+});
+
+if (module.parent) {
+  log.info("Llamada de ejecución como módulo")
+  module.exports = server
+}
+
