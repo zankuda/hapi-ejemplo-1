161371b42715e181a1c542423c8135a1ed8a5374
Harold Fritz <harold.fritz@zankuda.com>
2015-09-29 14:40:27 -0300

    Se genera nombre de archivo al azar, usando *rand-token* y manteniendo la extensión original del archivo.

--- routes.js
+++ routes.js
@@ -37,9 +37,11 @@ exports.init = function (server) {
       var fs = require('fs')
       // Funciones de manejo de rutas de arhcivos
       var path = require('path')
+      // Generador de textos al azar.
+      var randtoken = require('rand-token')
       if (request.payload.file) {
         var file = {
-          name: request.payload.file.filename,
+          name: randtoken.generate(60) + path.extname(request.payload.file.filename), // Se genera un texto de 60 caráteres y concatena la extensión del archivo
           contentType: request.payload.file.headers['content-type'],
           length: request.payload.file.bytes
         }
